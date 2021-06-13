44f949fad3d5b6ee67cb96ee6df140022f619821
Keniak1-G9 <84326221+Keniak1-G9@users.noreply.github.com>
2021-06-13 00:10:09 -0500

    Improved the Player's movement and small changes to the platform prefab
    Not excellent but it's good, pretty good i would say.

--- Rope/Assets/Scripts/PlayersMovement.cs
+++ Rope/Assets/Scripts/PlayersMovement.cs
@@ -11,8 +11,8 @@ public class PlayersMovement : MonoBehaviour
     public Transform groundCheckP2;
 
     const float speed = 15.0f;
-    const float jumpForce = 90.0f;
-    const float maximumVelocity = 20.0f;
+    const float jumpForce = 150.0f;
+    const float maximumVelocity = 10.0f;
 
 
     // Update is called once per frame
@@ -21,9 +21,10 @@ public class PlayersMovement : MonoBehaviour
         // This makes a raycast in order to know if he's touching the ground in order to jump, (because it doesn't make sense if he's in the
         // air and jumping mid air, bruh).
         // Ground Checks for Player 1 position
-        RaycastHit2D hit = Physics2D.Raycast(groundCheckP1.position, -Vector2.up, 0.2f);
+        RaycastHit2D hit = Physics2D.Raycast(groundCheckP1.position, -Vector2.up, 0.35f);
+
         // Ground checks for player 2 position
-        RaycastHit2D hit2 = Physics2D.Raycast(groundCheckP2.position, -Vector2.up, 0.2f);
+        RaycastHit2D hit2 = Physics2D.Raycast(groundCheckP2.position, -Vector2.up, 0.35f);
 
 
         #region Player 1 Controls
@@ -41,6 +42,16 @@ public class PlayersMovement : MonoBehaviour
         // D is for going to the right
         if (Input.GetKey(KeyCode.D)) { player1.AddForce(new Vector2(speed, 0)); }
         // And there is no need for need for the players to get a S key for going down
+
+        // Holy shit it finally works, it only took 3 tries to finding a code that actually works, I copied this one from the 2010
+        // unity forum's question, the code was given by "3itg" from a 2014 response comment. (Many Thanks to that guy)
+        if (Mathf.Abs(player1.velocity.x) > maximumVelocity)
+         {
+            // clamp velocity:
+            Vector3 newVelocity = player1.velocity.normalized;
+            newVelocity *= maximumVelocity;
+            player1.velocity = newVelocity;
+        }
         #endregion
 
 
@@ -59,13 +70,14 @@ public class PlayersMovement : MonoBehaviour
         // Right Arrow is going to the right (bruh)
         if (Input.GetKey(KeyCode.RightArrow)) { player2.AddForce(new Vector2(speed, 0)); }
         // And there is no need for need for the players to get a downArrow key for going down
+
+        if (Mathf.Abs(player2.velocity.x) > maximumVelocity)
+        {
+            // clamp velocity:
+            Vector3 newVelocity = player2.velocity.normalized;
+            newVelocity *= maximumVelocity;
+            player2.velocity = newVelocity;
+        }
         #endregion
     }
-    // This is only used for the players Terminal Velocity.
-    void FixedUpdate()
-    {
-        player1.velocity = Vector2.ClampMagnitude(player1.velocity, maximumVelocity);
-        player2.velocity = Vector2.ClampMagnitude(player2.velocity, maximumVelocity);
-    }
-
 }
