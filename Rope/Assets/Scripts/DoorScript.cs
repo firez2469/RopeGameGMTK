10f82252a1c15190f5a826cd0bca1fcce75e8028
firez2469 <47616467+firez2469@users.noreply.github.com>
2021-06-12 23:38:15 -0400

    Bug fixes

--- Rope/Assets/Scripts/DoorScript.cs
+++ Rope/Assets/Scripts/DoorScript.cs
@@ -16,10 +16,11 @@ public class DoorScript : MonoBehaviour
     void Update()
     {
         // When all the keys are collected, change the lock state of the door to "open"
-        if (KeyScript.TotalKeys() == KeyScript.KeysCollected) 
+        if (KeyScript.TotalKeys() == KeyScript.KeysCollected||KeyScript.TotalKeys()==0) 
         {
             doorLocked = false;
         }
+        print("Comarison:"+KeyScript.TotalKeys() + ":" + KeyScript.KeysCollected);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -30,12 +31,13 @@ public class DoorScript : MonoBehaviour
             Debug.Log("IT'S WORKING!!!, IT'S WORKING, WOOOOW, HELL YEAH!!");
             // make it so it loads the next scene.
             //LoadingScene.LoadScene(GameManager.NextLevel);
-        }
-        else
-        {
             this.GetComponent<SpriteRenderer>().sprite = open;
             GameManager.gameOver = true;
             collision.gameObject.SetActive(false);
+        }
+        else
+        {
+            
         }
     }
 }
